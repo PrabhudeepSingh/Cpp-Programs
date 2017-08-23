dc8c43936959a62d4c21034c3a23279d57f25408
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-23 22:32:32 +0530

    To illustrate concept of 2D Array.

---
+++ 2dArray.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char day[7][15]={"SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THRUSDAY","FRIDAY","SATURDAY"};
+	
+	for(int i=0;i<7;i++)
+		cout<<day[i]<<endl;
+	
+return 0;
+}
