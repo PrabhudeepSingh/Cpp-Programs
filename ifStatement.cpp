2455a38763dd2f45ed85429388498d98bcedf658
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-24 11:04:18 +0530

    To illustrate the use of if Statement.

---
+++ ifStatement.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+	int i=5;
+	
+	if(i==5)
+	{
+			cout<<"hello";
+			cout<<"HOW ARE U";	
+	}
+    
+    else
+		cout<<"hi";
+    
+return 0;
+}
+
