6094ed3a7e042fe2d575537450d8a9842162a598
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-26 13:10:01 +0530

    To illustrate the use of ws.

---
+++ ws.cpp
@@ -0,0 +1,14 @@
+//PROGRAM TO SHOW THE USE OF ws.
+#include<iostream>
+using namespace std;
+int main()
+{
+	char a[10];
+	
+	cout<<"ENTER THE NAME : ";
+	cin>>ws;
+	cin>>a;
+	cout<<"\nTHE NAME IS : "<<a;
+	
+return 0;
+}
