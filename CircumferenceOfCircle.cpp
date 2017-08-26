aa2452f12bac6db5166c70273e96feb15839fc9d
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-26 13:07:25 +0530

    To find the Circumference of a circle.

---
+++ CircumferenceOfCircle.cpp
@@ -0,0 +1,16 @@
+//PROGRAM TO FIND CIRCUMFERENCE OF A CIRCLE
+#include<iostream>
+using namespace std;
+int main()
+{
+	float r,c;
+	
+	cout<<"ENTER THE VALUE OF RADIUS : ";
+	cin>>r;
+	
+	c=2*3.14*r;
+	
+	cout<<"\nCIRCUMFERENCE OF CIRCLE = "<<c;
+	
+return 0;
+}
