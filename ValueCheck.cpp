95eeb550b7feb446415207148a2cfaad5d3a0e81
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-26 13:06:22 +0530

    To check the value of a variable

---
+++ ValueCheck.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y;
+	
+	cout<<"ENTER THE VALUE OF x : ";
+	cin>>x;
+	
+	if(x>0)
+		cout<<"\nVALUE OF y IS : 2";
+	
+	else
+		if(x==0)
+			cout<<"\nVALUE OF y IS : 0";
+		
+		else
+			cout<<"\nINVALID VALUE OF x.";
+
+return 0;
+}
