04e90dd02e0a97d03eed222474bdaa3181c868fd
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-27 11:06:07 +0530

    To illustrate the use of endl

---
+++ endl.cpp
@@ -0,0 +1,13 @@
+//PROGRAM TO SHOW THE USE OF endl
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=100;
+	double b=3.14;
+	
+	cout<<"a = "<<a<<endl;
+	cout<<"b = "<<b<<endl<<"a*b = "<<a*b;
+
+return 0;	
+}
