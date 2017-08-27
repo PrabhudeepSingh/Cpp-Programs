91c0e92e324ca35721078ce0c5ef707cc102ae4d
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-27 11:07:25 +0530

    to illustrate the use of flush Manipulator

---
+++ flush.cpp
@@ -0,0 +1,9 @@
+//PROGRAM TO SHOW THE USE OF flush MANIPULATOR
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"MY NAME IS COMPUTER\n";
+	cout<<"MANY GREETINGS TO YOU\n";
+	cout.flush();
+}
