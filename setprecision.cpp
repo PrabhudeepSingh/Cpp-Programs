251811ddff041219138accc76d09b85f205beaac
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-28 09:57:06 +0530

    To illustrate the use of  Manipulator.

---
+++ setprecision.cpp
@@ -0,0 +1,12 @@
+//PROGRAM TO SHOW THE USE OF setprecision MANIPULATOR
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	float x=0.11111;
+	cout<<setprecision(3)<<x<<endl;
+	cout<<setprecision(5)<<x<<endl;
+	
+return 0;
+}
