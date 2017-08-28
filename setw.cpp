817f3fc0474d667f9a1c06fa1eb7770e3c2dbcf7
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-28 09:55:55 +0530

    To illustrate the use of setw Manipulator.

---
+++ setw.cpp
@@ -0,0 +1,11 @@
+//PROGRAM TO SHOW USE OF setw MANIPULATOR
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	int a=200,b=300;
+	cout<<setw(10)<<a<<setw(5)<<b<<endl;
+	
+return 0;
+}
