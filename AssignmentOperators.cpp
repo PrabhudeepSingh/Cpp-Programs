f93181d9a6b2fc56820fa0f1cd189df38a58aee6
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-29 09:08:43 +0530

    To show the significance of Assignment Operators.

---
+++ AssignmentOperators.cpp
@@ -0,0 +1,20 @@
+//PROGRAM TO SHOW THE SIGNIFICANCE OF ASSIGNMENT OPERATORS
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	
+	a=10;
+	cout<<"a = "<<a;
+	
+	b=4;
+	cout<<"\nb = "<<b;
+	
+	a=b;
+	b=7;
+	
+	cout<<"\na = "<<a<<"\nb = "<<b;
+	
+return 0;
+}
