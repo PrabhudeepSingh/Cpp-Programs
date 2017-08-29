5984f1131a0404ffb9aa164854fde64c90a3837d
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-29 09:08:00 +0530

    To show the significance of Arithematic Operators.

---
+++ ArithematicOperators.cpp
@@ -0,0 +1,21 @@
+//PROGRAM TO SHOW THE SIGNIFICANCE OF ARITHEMATIC OPERATOR
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y,z;
+	
+	cout<<"ENTER THE 1st NUMBER : ";
+	cin>>x;
+	
+	cout<<"ENTER THE 2nd NUMBER : ";
+	cin>>y;
+	
+	cout<<endl<<"ADDITION : "<<x+y<<endl;
+	cout<<"SUBTRACTION : "<<x-y<<endl;
+	cout<<"MULTIPLICATION : "<<x*y<<endl;
+	cout<<"DIVISION : "<<x/y<<endl;
+	cout<<"MODULUS : "<<x%y;
+	
+return 0;
+}
