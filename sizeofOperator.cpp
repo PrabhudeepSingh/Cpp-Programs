91fdd3599c7be9de582604ebe7806288102617d9
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-30 10:24:41 +0530

    To show the significance of sizeof Operators.

---
+++ sizeofOperator.cpp
@@ -0,0 +1,19 @@
+//PROGRAM TO SHOW THE USE OF sizeof OPERATOR
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	float b;
+	double c;
+	char d;
+	
+	cout<<"\nSIZE OF a = "<<sizeof(a);
+	cout<<"\nSIZE OF b = "<<sizeof(b);
+	cout<<"\nSIZE OF c = "<<sizeof(a);
+	cout<<"\nSIZE OF d = "<<sizeof(d);
+	cout<<"\nSIZE OF long double = "<<sizeof(long double);
+	cout<<"\nSIZE OF float = "<<sizeof(float);
+	
+return 0;
+}
