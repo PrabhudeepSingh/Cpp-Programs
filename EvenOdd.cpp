92c137c65ced8120ee6fe17108320396efe2553c
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-31 21:32:10 +0530

    Program to find that a number is even or odd.

---
+++ EvenOdd.cpp
@@ -0,0 +1,17 @@
+//PROGRAM TO ENTER AN INTEGER AND FIND THE NUMBER IS EVEN AND ODD
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	
+	cout<<"ENTER AN INTEGER : ";
+	cin>>x;
+	
+	if(x%2==0)
+		cout<<"\nNUMBER IS EVEN";
+	else
+		cout<<"\nNUMBER IS ODD";
+		
+return 0;
+}
